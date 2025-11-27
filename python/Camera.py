176a8a405077c11519c62c068237f66e2d2af1fb
Gabriella S <scheldtgb@cardiff.ac.uk>
2025-11-27 22:06:54 +0000

    Reworked Python Structure

---
+++ python/Camera.py
@@ -0,0 +1,9 @@
+class Camera:
+    def __init__(self):
+        #self.camera = Picamera() - to implement
+        pass
+    
+    def capture_and_store(self, folder: str = "static/images"):
+        # will take picture, store, and give uniqueID for filepath
+        return file_path
+        
---
+++ python/main.py
@@ -0,0 +1,8 @@
+from .Image import Image
+from .Image import DisplayImage
+from .Image import TakeImage
+from .Image import QRScanImage
+from .Image import Gallery
+
+
+test_images - [D]
